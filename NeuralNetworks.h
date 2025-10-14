fc1b8bfc71706b71ce1410b1bb3b62913eb319fa
Jackson Nace <jacksonnace964@gmail.com>
2025-10-14 15:47:28 -0500

    init on mac

--- NeuralNetworks.h
+++ NeuralNetworks.h
@@ -6,6 +6,7 @@ using namespace std;
 struct Case {
   int diagnosis; // 1 for malignant, 0 for benign. We swap this data as it is originally M and B
   vector<double> features; // stuff in a double vector to make it easier to work with
+  // minor change to test commits
 };
 
 #endif
